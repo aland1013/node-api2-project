64c367960e16919057ecee571d70c65a7ae76182
aland1013 <adam.land@gmail.com>
2020-07-10 13:23:55 -0400

    finished DELETE /api/posts/:id endpoint

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -100,4 +100,51 @@ router.get('/:id', (req, res) => {
     });
 });
 
+/* ----- GET /api/posts/:id/comments ----- */
+router.get('/:id/comments', (req, res) => {
+  const { id } = req.params;
+
+  db.findById(id).then((post) => {
+    if (post.length === 0) {
+      res.status(404).json({
+        message: 'The post with the specified ID does not exist'
+      });
+    } else {
+      db.findPostComments(id)
+        .then((comments) => {
+          res.status(200).json(comments);
+        })
+        .catch((err) => {
+          res.status(500).json({
+            error: 'The comments information could not be retrieved'
+          });
+        });
+    }
+  });
+});
+
+/* ----- DELETE /api/posts/:id ----- */
+router.delete('/:id', (req, res) => {
+  // removes the post from the db and returns the deleted post object
+  const { id } = req.params;
+
+  db.findById(id).then((post) => {
+    if (post.length === 0) {
+      res.status(404).json({
+        error: 'The post with the specified ID does not exist'
+      });
+    } else {
+      db.remove(id)
+        .then((numRecs) => {
+          res.status(200).json(post);
+        })
+        .catch((err) => {
+          res.status(500).json({
+            error: 'The post could not be removed'
+          });
+        });
+    }
+  });
+});
+
 module.exports = router;
