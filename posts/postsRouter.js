43ad482e673a437a72ef772fb73bcdb76125b760
aland1013 <adam.land@gmail.com>
2020-07-10 01:23:06 -0400

    finished another endpoint

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -23,4 +23,51 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/:id/comments', (req, res) => {
+  const date = new Date().toString();
+
+  const comment = {
+    ...req.body,
+    post_id: req.params.id,
+    created_at: date,
+    updated_at: date
+  };
+
+  db.findById(req.params.id).then((post) => {
+    if (post.length === 0) {
+      res.status(404).json({
+        message: 'The post with the specified ID does not exist'
+      });
+    } else if (!comment.text) {
+      res.status(400).json({
+        errorMessage: 'Please provide text for the comment'
+      });
+    } else {
+      db.insertComment(comment)
+        .then((newComment) => {
+          res.status(201).json(comment);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({
+            error: 'There was an error while saving the comment to the database'
+          });
+        });
+    }
+  });
+});
+
+router.get('/', (req, res) => {
+  db.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: 'The posts information could not be retrieved'
+      });
+    });
+});
+
 module.exports = router;
